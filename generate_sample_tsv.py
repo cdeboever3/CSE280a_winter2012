3b7f4af9ef26f1ae421b1b335c6147d6790575e0
Chris DeBoever <cdeboever3@gmail.com>
2012-03-06 09:25:37 -0800

    Added noise sites to sample tsv script
    See description with python generate_sample_tsv.py -h

--- generate_sample_tsv.py
+++ generate_sample_tsv.py
@@ -31,7 +31,9 @@ if __name__ == '__main__':
 
     ploidyL = [2,2]
     subL = [0.25,0.75]
-    num_sites = 100
+    num_sites = 200
+    num_noise_sites = 10
+    error_rate = 0.01
     avg_cov = 50
     std_cov = 10
 
@@ -40,19 +42,23 @@ if __name__ == '__main__':
     parser.add_argument('-pL', default=ploidyL, nargs='+', help='A list of ploidies. Each entry in the list represents the anticipated ploidy of a subpopulation. For instance, if you expect two diploid subpopulations and one triploid subpopulation, enter 2 2 3. Default: {0}'.format(' '.join([str(x) for x in ploidyL])))
     parser.add_argument('-sL', default=subL, nargs='+', help='List of subpopulation frequencies. Default: {0}'.format(' '.join([ str(x) for x in subL ])))
     parser.add_argument('-ns', default=num_sites, type=int, help='Number of variable sites. Default: {0}'.format(num_sites))
+    parser.add_argument('-nn', default=num_noise_sites, type=int, help='Number of variable sites that are noise. Default: {0}'.format(num_noise_sites))
+    parser.add_argument('-er', default=error_rate, type=int, help='Error rate for noise sites. Default: {0}'.format(error_rate))
     parser.add_argument('-ac', default=avg_cov, type=int, help='Average coverage. Default: {0}'.format(avg_cov))
     parser.add_argument('-sc', default=std_cov, type=int, help='Standard deviation of coverage. Default: {0}'.format(std_cov))
     parser.add_argument('-d', action='store_true', help='Enable python debugger.')
     
     args = parser.parse_args()
    
-    outF        = args.o
-    ploidyL     = args.pL
-    subL        = args.sL
-    num_sites   = args.ns
-    avg_cov     = args.ac
-    std_cov     = args.sc
-    debug       = args.d
+    outF            = args.o
+    ploidyL         = args.pL
+    subL            = args.sL
+    num_sites       = args.ns
+    num_noise_sites = args.nn
+    error_rate      = args.er
+    avg_cov         = args.ac
+    std_cov         = args.sc
+    debug           = args.d
 
     if len(ploidyL) > 2:
         print >>sys.stderr, 'Sorry, only two subpopulations are currently supported.'
@@ -60,9 +66,14 @@ if __name__ == '__main__':
 
     exp_freqL = generate_possible_freqL(ploidyL,subL)[1:] # we don't want 0 frequency
 
-    for iii in range(num_sites):
+    for iii in range(num_sites-num_noise_sites):
         cov = int(round(gauss(avg_cov,std_cov)))
         alt_freq = random.choice(exp_freqL)
         alt_reads = binomial(cov,alt_freq)
+
+        print >>outF, '{0}\t{1}'.format(cov-alt_reads,alt_reads)
+    for iii in range(num_noise_sites):
+        cov = int(round(gauss(avg_cov,std_cov)))
+        alt_reads = binomial(cov,error_rate)
 
         print >>outF, '{0}\t{1}'.format(cov-alt_reads,alt_reads)
