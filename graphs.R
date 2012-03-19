f7981af4d484e093d6f0498a015eef9806f469b9
Michael Yu <kenlo1959@gmail.com>
2012-03-19 01:25:37 -0700

    Updated graph plotting script graphs.R

--- graphs.R
+++ graphs.R
@@ -4,22 +4,24 @@ parse.table <- function(filename) {
   return(as.matrix(read.table(filename, check.names=FALSE)))
 }
 
-tp = parse.table('Figures/coverage_vs_alpha.tp.txt')
-fp = parse.table('Figures/coverage_vs_alpha.fp.txt')
-fn = parse.table('Figures/coverage_vs_alpha.fn.txt')
-tn = parse.table('Figures/coverage_vs_alpha.tn.txt')
+## tp = parse.table('Figures_email/coverage_vs_alpha.tp.txt')
+## fp = parse.table('Figures_email/coverage_vs_alpha.fp.txt')
+## fn = parse.table('Figures_email/coverage_vs_alpha.fn.txt')
+## tn = parse.table('Figures_email/coverage_vs_alpha.tn.txt')
 
-tp.rate = tp / (tp + fn)
-fp.rate = fp / (tn + fp)
+## tp.rate = tp / (tp + fn)
+## fp.rate = fp / (tn + fp)
   
-alpha = parse.table('Figures/coverage_vs_alpha.alpha.txt')
+## alpha = abs(parse.table('Figures_email/coverage_vs_alpha.alpha.txt'))
 
-files <- list(list(table=tp.rate, filename='Figures/coverage_vs_alpha.tp_rate', title='True Positive Rate'),
-              list(table=fp.rate, filename='Figures/coverage_vs_alpha.fp_rate', title='False Positive Rate'),
-              list(table=fp, filename='Figures/coverage_vs_alpha.fp', title='# of False Positives'),
-              list(table=fn, filename='Figures/coverage_vs_alpha.fn', title='# of False Negatives'),
-              list(table=alpha, filename='Figures/coverage_vs_alpha.alpha', title='Alpha Error\n = |Actual alpha - Predicted Alpha|'))
+## files <- list(list(table=tp.rate, filename='Figures_email/coverage_vs_alpha.tp_rate', title='True Positive Rate'),
+##               list(table=fp.rate, filename='Figures_email/coverage_vs_alpha.fp_rate', title='False Positive Rate'),
+##               list(table=fp, filename='Figures_email/coverage_vs_alpha.fp', title='# of False Positives'),
+##               list(table=fn, filename='Figures_email/coverage_vs_alpha.fn', title='# of False Negatives'),
+##               list(table=alpha, filename='Figures_email/coverage_vs_alpha.alpha', title='Alpha Error\n = |Actual alpha - Predicted Alpha|'))
 
+alpha = abs(parse.table('Figures_email/coverage_vs_alpha_improved.alpha.txt'))
+files <- list(list(table=alpha, filename='Figures_email/coverage_vs_alpha_improved.alpha',title='Alpha Error\n= |Actual alpha - Predicted alpha|'))
 
 for (i in seq(along=files)) {
   graph.table = files[[i]][['table']]
